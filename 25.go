a3ba65bcc4dc5a3e10e4f98d1d5589fdde3d30ca
baralex87 <baralex1987@gmail.com>
2024-11-21 13:27:05 +0300

    Corrections on input request

--- 25.go
+++ 25.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	var n string
-	fmt.Print("Введите целое число: ")
+	fmt.Print("Введите нужные данные: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %v.\n", n)
+	fmt.Printf("Вы ввели такие данные: %v.\n", n)
 }
